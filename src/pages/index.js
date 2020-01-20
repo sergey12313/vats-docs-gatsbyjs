4c181756c69451dbb8f8abb9b40d00a5c08c5871
sergey12313 <sergey12313@gmail.com>
2020-01-20 22:35:23 +0300

    refactoring qraphql query

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,21 +2,14 @@ import React from "react"
 import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 export default ({ data }) => {
-  // console.log(data)
   const post = data.mdx
   return (
-      <>
-       fsdfsdfsdf
         <MDXRenderer>{post.body}</MDXRenderer>
-    </>
   )
 }
 export const query = graphql`
 query{
   mdx(fileAbsolutePath: {regex: "/^.*introduction/README.mdx$/"}) {
-    frontmatter {
-      title
-    }
     body
   }
 }
